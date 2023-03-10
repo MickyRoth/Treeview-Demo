9d56548f4af801a1d38f4d71a3d59b857f808044
Michael Roth <roth@wifa.de>
2023-03-10 14:43:41 +0100

    Ohne Größenveränderung

--- Treeview-Demo.py
+++ Treeview-Demo.py
@@ -2,6 +2,7 @@
 from tkinter import *
 from tkinter import ttk
 win = Tk()
+win.resizable(width=False, height=False)
 
 spalten=["0","1","2"]
 tv = ttk.Treeview(win, columns=spalten, height=3, show="headings")
@@ -11,8 +12,8 @@ tv.heading("1", text="Nachname")
 tv.heading("2", text="Kontostand")
 
 tv.column("0", width=75)
-tv.column("1", width=75, anchor = "center")
-tv.column("2", width=70, anchor = "center")
+tv.column("1", width=75, anchor = "w")
+tv.column("2", width=70, anchor = "e")
 
 ds0=["Hans", "Hansmann",345]
 ds1=["Otto", "Knarrsack",250]
